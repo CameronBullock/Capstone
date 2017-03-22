bbb9334e0aec10b442681e3b943f22aef7412852
Cameron Bullock <bullockcameron@gmail.com>
2017-03-21 18:12:51 -0600

    Jake got the random index working witht he buttin

--- client/components/Article.js
+++ client/components/Article.js
@@ -5,15 +5,16 @@ import ReactDom from "react-dom";
 class Article extends React.Component{
   constructor(props){
     super(props);
+    this.state = { randomArticleNumber: this.props.allArticles.websites.length -1 }
   }
 
   newArticle = () => {
-    let randomArticleNumber = Math.floor(Math.random() * this.props.allArticles.websites.length);
+    this.setState({ randomArticleNumber: Math.floor(Math.random() * this.props.allArticles.websites.length) });
   }
   render(){
     return(
       <div className="container center">
-          <iframe src={this.props.allArticles.websites[3].url} width="100%" height="800px"></iframe>
+          <iframe src={this.props.allArticles.websites[this.state.randomArticleNumber].url} width="100%" height="800px"></iframe>
           <button className=" btn" onClick={this.newArticle}>Next Article</button>
       </div>
     )
